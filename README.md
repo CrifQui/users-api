4d11ca44bbfa089f79286b814df96b6d14acfe7d
Cristian QUINAYAS <cristian@sinchicreaciones.com>
2025-12-14 16:37:02 -0500

    implement server express with routes

---
+++ README.md
@@ -0,0 +1,2 @@
+cors library is to cross-origin requirements
+morgan loggin api (dev/prod)
---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import cors from "cors"; // libreria para manejar solicitudes entre diferentes dominios (cross-origin)
+import { errorMiddleware } from "./middlewares/error.middleware";
+import express from "express";
+import morgan from "morgan"; // libreria para registro de solicitudes http
+import routes from "./routes";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+
+app.get("/health", (req, res) => {
+    res.status(200).send({ status: "OK" });
+});
+app.use("/api", routes);
+
+// manejo de errores al final siempre best preactice
+app.use(errorMiddleware);
+export default app;
