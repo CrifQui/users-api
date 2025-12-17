f902c18b32ab19f64ee327b2c6d0bb39de45d93b
Cristian QUINAYAS <cristian@sinchicreaciones.com>
2025-12-17 14:10:31 -0500

    add auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+
+import jwt from "jsonwebtoken";
+
+interface JwtPayload {
+    sub: string;
+    role: string;
+}
+
+export const authMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const header = req.headers.authorization;
+
+    if (!header || !header.startsWith("Baerer "))
+        return res.status(401).json({ message: "Unauthorized" });
+
+    const token = header.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET as string
+        ) as JwtPayload;
+
+        req.user = {
+            id: decoded.sub,
+            role: decoded.role,
+        };
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+};
