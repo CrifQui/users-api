6c240b17d82cfefbc673d0115e603cdb5f9d4695
Cristian QUINAYAS <cristian@sinchicreaciones.com>
2025-12-18 16:34:26 -0500

    add refresh test

---
+++ src/tests/refresh.test.ts
@@ -0,0 +1,34 @@
+import app from "../app.js";
+import { prisma } from "../config/prisma.js";
+import request from "supertest";
+
+describe("Auth refresh", () => {
+    let refreshToken: string;
+    beforeAll(async () => {
+        const result = await request(app).post("/api/auth/login").send({
+            email: "test@test.com",
+            password: "P@ssword123",
+        });
+        refreshToken = result.body.refreshToken;
+    });
+
+    it("should return tokens", async () => {
+        const result = await request(app)
+            .post("/api/auth/refresh")
+            .send({ refreshToken });
+        expect(result.status).toBe(200);
+        expect(result.body.accessToken).toBeDefined();
+    });
+
+    it("should fail with revoked token", async () => {
+        await prisma.refreshToken.updateMany({
+            where: { token: refreshToken },
+            data: { revoked: true },
+        });
+        const result = await request(app)
+            .post("/api/auth/refresh")
+            .send({ refreshToken });
+        expect(result.status).toBe(401);
+        expect(result.body.message).toBe("Invalid refresh token");
+    });
+});
