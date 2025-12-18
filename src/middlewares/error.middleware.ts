c464574ea28f951a1f01de5a6e61ef6749a3a252
Cristian QUINAYAS <cristian@sinchicreaciones.com>
2025-12-18 15:31:03 -0500

    add error handler

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,10 +1,18 @@
 import { NextFunction, Request, Response } from "express";
+
+import { AppError } from "../utils/errorHandler";
+
 export const errorMiddleware = (
     err: Error,
-    _req: Request,
+    req: Request,
     res: Response,
-    _next: NextFunction
+    next: NextFunction
 ) => {
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            message: err.message,
+        });
+    }
     console.error(err);
-    res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ message: "Internal Server Error" });
 };
